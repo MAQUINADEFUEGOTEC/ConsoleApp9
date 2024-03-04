dcc50e3ca2dd56e55d6acb974eb9cdba583fedaa
inicio <inicio@FISEP17>
2024-03-04 14:28:53 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Escuela_de_computacion_actividad_2;
+
+Fecha fecha = new Fecha();
+
+fecha.modificarFecha();
+fecha.MostrarFecha();
+fecha.MostrarFechaPalabras();
